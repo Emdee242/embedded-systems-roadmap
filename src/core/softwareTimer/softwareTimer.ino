d41694ece5caa6147a8652ffde78a95b4d6a051a
Atta Mahmoud Adeiza <attamahd2023@gmail.com>
2026-08-25 11:54:55 +0100

    completed my software timer

--- src/core/softwareTimer/softwareTimer.ino
+++ src/core/softwareTimer/softwareTimer.ino
@@ -1,3 +1,21 @@
+class Timer{
+  private:
+  constexpr unsigned long intervalTime;
+  unsigned long recordTime = 0;
+  public:
+  Timer (unsigned long x) : intervalTime(x){}
+  bool interval(){
+    bool result = false;
+    if((millis() - recordTime) >= intervalTime){
+      recordTime = millis();
+      result = true;
+    }
+    return result;
+  }
+};
 void setup(){
   
+}
+void loop(){
+
 }
