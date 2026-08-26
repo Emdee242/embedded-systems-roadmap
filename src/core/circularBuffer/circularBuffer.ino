187b0f4a795a9b28db9eddd72bf5c8b6c4c639f9
Atta Mahmoud Adeiza <attamahd2023@gmail.com>
2026-08-27 00:29:18 +0100

    if the commit is successful, it will add almost all structure to my circular buffer

--- src/core/circularBuffer/circularBuffer.ino
+++ src/core/circularBuffer/circularBuffer.ino
@@ -1,14 +1,40 @@
+#include <iostream>
 class Buffer{
 private:
-int size = 1024;
+constexpr static int size = 8;
 int buffArray[size];
 int head = 0;
 int tail = 0;
+bool isArrayFull = false;
 public:
-if(head == tail){
-head = (head + 1) & (size - 1);
-}
-int write(){
+void write(int x){
+  buffArray[head] = x;
+  if((tail & (size - 1)) == (head & (size - 1))){
+    isArrayFull = true;
+  }
+  head = (head + 1) & (size - 1);
   
 }
+int read(){
+  int readVar;
+  readVar = buffArray[tail];
+  if((head & (size - 1)) == (tail & (size - 1))){
+    isArrayFull = false;
+  }
+  if(isFull() == false){
+    return 0;
+  }else{
+    tail = (tail + 1) & (size - 1);
+  return readVar;
+  }
+}
+bool isFull() const{
+  return isArrayFull;
+}
 };
+void setup(){
+
+}
+void loop(){
+
+}
