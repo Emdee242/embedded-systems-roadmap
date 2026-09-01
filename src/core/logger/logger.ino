431c03bd529b1f79cec5181ad24b6f9018a36b2d
Emdee242 <attamahd2023@gmail.com>
2026-09-01 13:03:04 +0100

    if successful, commit will add the logger project

--- src/core/logger/logger.ino
+++ src/core/logger/logger.ino
@@ -1,1 +1,29 @@
+#include <iostream>
+enum class Severity{
+  INFO,
+  WARN,
+  ERROR
+};
+void Log(const char* message, Severity Level){
+Serial.print(message); Serial.print("      ");
+switch(Level){
+  case(Severity::INFO):
+Serial.println("INFO");
+break;
+case(Severity::WARN):
+Serial.println("WARN");
+  break;
+case(Severity::ERROR):
+Serial.println("ERROR");
+  break;
+  default:
+  std::cout << "Invalid input";
+  break;
+}
+}
+void setup(){
 
+}
+void loop(){
+
+}
