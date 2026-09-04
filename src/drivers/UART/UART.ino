ee1d4d3b75d2bfb3787b4e478b8d7b865c90df97
Emdee242 <attamahd2023@gmail.com>
2026-09-04 13:23:41 +0100

    Finish the UART echo project in drivers

--- src/drivers/UART/UART.ino
+++ src/drivers/UART/UART.ino
@@ -1,1 +1,13 @@
-
+void setup(){
+Serial.begin(9600);
+}
+char incomingString;
+void loop(){
+if(Serial.available()){
+  incomingString = Serial.read();
+  Serial.print(incomingString);
+  if(incomingString == '\n'){
+    Serial.println();
+  }
+}
+}
