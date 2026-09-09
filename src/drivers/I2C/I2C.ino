7c4c6e7292008cc874dade45c65bdfd6521d3eba
Emdee242 <attamahd2023@gmail.com>
2026-09-09 15:58:45 +0100

    initial build of the mpu6000 driver code block

--- src/drivers/I2C/I2C.ino
+++ src/drivers/I2C/I2C.ino
@@ -1,1 +1,13 @@
-
+void setup(){
+Serial.begin(9600);
+Wire.begin();
+}
+void loop(){
+int wire = 0x75;
+int mpuAddress = 0x68;
+int consoleOutput;
+Wire.beginTransmission(mpuAddress);
+Wire.requestFrom(mpuAddress, wire);
+consoleOutput = Wire.read();
+Serial.print(consoleOutput);
+}
